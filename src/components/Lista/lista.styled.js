c10c31885bde3ce97ab3ebed53b048e2b087228d
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-07-06 21:43:58 -0300

    clase 6: Parte A emotion styled childs

---
+++ src/components/Lista/lista.styled.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled"
+import { CardContainer } from "../Card/card.styled";
+
+export const CardsContainer = styled.ul`
+    display: flex;
+    gap: 1rem;
+    list-style: none;
+`;
